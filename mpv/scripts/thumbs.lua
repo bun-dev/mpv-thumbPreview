73548ccb10f315d97e71e74d2c8ed19126fdc82c
bun-dev <34141271+bun-dev@users.noreply.github.com>
2017-12-05 20:06:55 -0500

    using tick instead of timer

--- mpv/scripts/thumbs.lua
+++ mpv/scripts/thumbs.lua
@@ -1,6 +1,9 @@
 --[[
-    MPV Thumbnail Preview
-    Version: 0.4
+	MPV Thumbnail Preview
+	Version: 0.4
+	
+	Generates preview thumbnails for mpv either locally or dynamically using overlay-add.	
+	Special thanks to various anons in https://boards.4chan.org/g/catalog#s=mpv
 --]]
 
 local msg = require 'mp.msg'
@@ -28,78 +31,93 @@ local hash = ""
 local init,init2 = false
 local regen = 0
 local oldSize= {}
-local thumb_height
+local thumb_height=0
 local duration = 0
 local thumbaddr = {}
 local offset_x_seekbar = (_global.thumb_width/2) --Center of thumbnail width.
-local zoneY, zoneX = 0		
 local zRect = {}
 local sw,sh = 0
 
+local function exists(file)
+	local ok, err, code = os.rename(file, file)
+	 if not ok then
+		if code == 13 then
+		 return true
+		end
+	 end
+	 return ok, err
+end
+
+local function isdir(path)
+	 return exists(path.."/")
+end
+
 local function script_path()
-   local str = debug.getinfo(2, "S").source:sub(2)
-   return str:match("(.*/)")
+	 local str = debug.getinfo(2, "S").source:sub(2)
+	 return str:match("(.*/)")
 end
 
 function GetFileExtension(strFilename)
-  return string.match(strFilename,"^.+(%..+)$")
+	return string.match(strFilename,"^.+(%..+)$")
 end
 
 local function escape(str)
-    return str:gsub("\\", "\\\\"):gsub("'", "'\\''")
+	return str:gsub("\\", "\\\\"):gsub("'", "'\\''")
 end
 
 local function osd(str)
-    return mp.osd_message(str, 2)
+	return mp.osd_message(str, 2)
 end
 
 -- modified from https://github.com/occivink/mpv-scripts/blob/master/drag-to-pan.lua
 local function compute_video_dimensions()
-    local video_params = mp.get_property_native("video-out-params")
-    local h = video_params["h"]
-    local dw = video_params["dw"]
-    local dh = video_params["dh"]
-
+	local video_params = mp.get_property_native("video-out-params")
+	local h = video_params["h"]
+	local w = video_params["w"]
+	local dw = video_params["dw"]
+	local dh = video_params["dh"]
+	 	
 	local fwidth = osd_w
 	local fheight = math.floor(osd_w / dw * dh)
-			
+		
 	if fheight > osd_h or fheight < h then
 		local tmpw = math.floor(osd_h / dh * dw)
+		
 		if tmpw <= osd_w then
-			fheight = osd_h
+			fheight = osd_h			
 			fwidth = tmpw
 		end
 	end
-	
+
 	if fheight < osd_h then
 		fheight = osd_h
 	end
 	
 	sw = fwidth + math.floor( fwidth / fheight)
-	sh = fheight + math.floor( fwidth / fheight)
+	sh = fheight +math.floor( fwidth / fheight)
+
 end
 
 local function resized()
 	oldSize.x,oldSize.y = osd_w,osd_h
 	compute_video_dimensions()
-	local osc = tostring(mp.get_property("osc"))
+	local osc = mp.get_property("osc")
 	
-	--hard coded ranges based on bottombar layout and progressbar.lua
-	if osc == "yes" then 
+--	osd(tostring(offset))
+	if osc then 
 		zRect = {
-			aY = ((sh * 94) / 100),
-			aY2 = ((sh * 1) / 100),
-			bX = ((sw * 19)/ 100),
-			bX2 = ((sw * 32) / 100)
+			aY = sh * 94/ 100,
+			aY2 = sh * 1/ 100,
+			bX =  sw * 18/ 100,
+			bX2 = sw * 32 / 100
 		}
 	else
 		zRect = {
-			aY = ((osd_h * 94) / 100),
-			aY2 = ((osd_h * 1)/ 100),
-			bX = ((osd_w* 1) / 100),
-			bX2 = ((osd_w * 1) / 100)
+			aY = sh * 94/ 100,
+			aY2 = sh * 1/ 100,
+			bX = sw* 1/ 100,
+			bX2 = sw * 1 / 100
 		}
-
 	end
 end
 
@@ -108,19 +126,18 @@ local function zone(p)
 	local v = {0, 1, 2}
 	local h = {0, 1, 2}
 	
-    local y = (p.y < zRect.aY) and v[1] or (p.y < (osd_h - zRect.aY2)) and v[2] or v[3]
+	local y = (p.y < zRect.aY) and v[1] or (p.y < (osd_h - zRect.aY2)) and v[2] or v[3]
 	local x = (p.x < zRect.bX) and h[1] or (p.x < (osd_w - zRect.bX2)) and h[2] or h[3]
 
-    return y, x
+	return y, x
 end
 
 local function on_seek()
-	--	init2 is set true when the thumbs creation has been started. we don't want these calculations being performed needlessly, unless caching is enabled.
-	if init2 or _global.cache then 
+	if init2 then 
 		osd_w,osd_h = mp.get_osd_size()
 		local point = {}
 		point.x, point.y = mp.get_mouse_pos()
-	
+		
 		--Setup video w/h once.
 		if vid_w == 0 then
 			vid_w,vid_h = mp.get_property("width"),mp.get_property("height")
@@ -136,8 +153,9 @@ local function on_seek()
 			resized()
 		end
 		
-		zoneY, zoneX = zone(point)
-		
+		local zoneY, zoneX = zone(point)
+			
+--		osd(tostring(point.x)..","..tostring(point.y)) --[Debug]			
 --		osd(tostring(zoneX).."-"..tostring(zoneY)) --[Debug] 
 		
 		local posx = (point.x-offset_x_seekbar)
@@ -145,10 +163,20 @@ local function on_seek()
 		
 		local region = zoneX ==1 and zoneY == 1 --mouse zone(seekbar)
 		local norm = math.floor(((point.x - zRect.bX)/ ((osd_w - zRect.bX2) - zRect.bX))*duration) --normalized range of mouse x(ssp)
-		local index = math.floor(norm/_global.timespan) +1
+		local index = math.floor(norm/_global.timespan) 
 		local thumbpath = nil
-						
-		if init then
+			
+		if init then	
+			--non-negative index. doesn't serve any particular purpose.
+			if index < 0 then
+				index = 0
+			end
+			
+			--cap index at table max.
+			if index > #thumbaddr+1 then	
+				index = #thumbaddr
+			end
+		
 			if _global.cache then
 				local tmp = string.format("%s%s",outpath,"\\thumb")
 				thumbpath = tmp..tostring(index)..".bgra"
@@ -156,18 +184,13 @@ local function on_seek()
 			else
 				thumbpath = "&"..tostring(thumbaddr[index])
 			end
-			
-			if index > #thumbaddr+1 then
-				index = #thumbaddr
-				mp.commandv("overlay-add",1,posx,posy,mpath.."blank.bgra", 0, "bgra",_global.thumb_width, thumb_height, (4*_global.thumb_width))
-			end
-			
-			if index < 0 then
-				index = 0
-			end
-						
+								
 			if region then
-				mp.commandv("overlay-add",1,posx,posy,thumbpath, 0, "bgra",_global.thumb_width, thumb_height, (4*_global.thumb_width))
+				if thumbaddr[index] ~= nil then
+					mp.commandv("overlay-add",1,posx,posy,thumbpath, 0, "bgra",_global.thumb_width, thumb_height, (4*_global.thumb_width))
+				else
+					mp.commandv("overlay-add",1,posx,posy,mpath.."blank.bgra", 0, "bgra",_global.thumb_width, thumb_height, (4*_global.thumb_width))
+				end
 			else
 				mp.commandv("overlay-remove",1)
 			end
@@ -196,21 +219,43 @@ local function checkThumbs()
 			osd("Video duration less than ".. tostring(_global.minTime) .. " seconds. Cancelled")
 		end
 	return end
-	
+		
 	--Check if a local folder exists or not if caching, else use regular streaming method.
 	if _global.cache then
-		local command = {}
-		command.args = {
-			"cd",outpath
+	
+		--Get MD5
+		local cmd = {}
+
+		cmd.args = {
+			"mpv",
+			"--msg-level","all=no",	
+			input,
+			"--frames","1",
+			"--t","0.1",
+			"--of", "md5",
+			"--o=-"
 		}
+
+		local process = utils.subprocess(cmd)
+		local tmp = process.stdout
+		hash = tostring(tmp:gsub("MD5=", ""))
+		hash = tostring(hash:gsub("\n", ""))
+		tmp = _global.thumbdir:gsub("\"", "")
+		outpath = tmp .. hash
+			
+		local status = isdir(outpath)
 		
-		local response = utils.subprocess(command)
-		
-		if response.status ~= 0 then
-			createThumbs()
+		if status then
+			if _global.auto then
+				init2 = true
+			else
+				osd("Thumbnails exist. Recreate? Y/N",25)
+				addBinding()		
+			end
+
 		else
-			osd("Thumbnails exist. Recreate? Y/N",25)
-			addBinding()
+			mp.msg.warn("Hash for video not found. Generating cache thumbs.")
+			createThumbs()
 		end	
 	else
 		if not init2 then
@@ -253,13 +298,10 @@ local function removeBinding()
 end
 
 local function unregister()
-	mp.msg.debug("mpv thumbs unregistered")
 	init = false
 	init2 = false
 	mp.remove_key_binding("checkThumbs")
 	mp.unregister_script_message("add_thumb")
-	timer:kill()
-	return
 end
 
 local check
@@ -275,6 +317,7 @@ check = function()
 	
 	--if the video isn't in the valid list, then it's ignored.
 	if not string.match(validlist, ext) then
+		mp.msg.warn("invalid file")
 		unregister()
 	return end
 	
@@ -284,34 +327,14 @@ check = function()
 		
 		--if the video length is too small, don't create thumbs.
 		if duration < _global.minTime then
+			mp.msg.warn("duration too small")
 			unregister()
 		return end
 	end
 		
 	oldSize.x,oldSize.y = mp.get_osd_size()
 	input = escape(utils.join_path(utils.getcwd(),mp.get_property("path")))
-	
-	--If we're using cache, grab md5 of video.
-	if _global.cache then
-		local cmd = {}
-
-		cmd.args = {
-			"mpv",
-			"--msg-level","all=no",	
-			input,
-			"--frames","1",
-			"--of", "md5",
-			"--o=-"
-		}
-
-		local process = utils.subprocess(cmd)
-		local tmp = process.stdout
-		hash = tostring(tmp:gsub("MD5=", ""))
-		hash = tostring(hash:gsub("\n", ""))
-		outpath = _global.thumbdir .. hash
-		init = true
-	end
-	
+		
 	if _global.auto then
 		checkThumbs()
 	end
@@ -325,10 +348,11 @@ mp.add_key_binding("k", "checkThumbs", checkThumbs)
 
 --registers
 mp.register_script_message("add_thumb", add_thumb)
-local timer = mp.add_periodic_timer(0.03, on_seek)
+mp.register_event("tick", on_seek)
 
 local fileLoaded
 fileLoaded = function()
-  return mp.register_event('playback-restart', check)
+	mp.unregister_event(fileLoaded)
+	return mp.register_event('playback-restart', check)
 end
 return mp.register_event('file-loaded', fileLoaded)
